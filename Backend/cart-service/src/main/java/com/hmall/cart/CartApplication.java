08fdb5912c27c12ab0b8883ff6abf94f2b7ed1fc
Li Tianyu <719471785@qq.com>
2024-05-03 12:32:18 +0800

    拆分购物车服务，实现购物车服务远程调用物品服务

---
+++ Backend/cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -0,0 +1,24 @@
+package com.hmall.cart;
+
+import com.hmall.common.domain.R;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author lty
+ */
+@MapperScan("com.hmall.cart.mapper")
+@SpringBootApplication
+public class CartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
