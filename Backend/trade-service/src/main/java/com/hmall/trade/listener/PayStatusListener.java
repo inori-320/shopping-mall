a39efd63aed9a67e6a1f7b138fe22a27423969e2
Li Tianyu <719471785@qq.com>
2024-05-09 18:11:03 +0800

    完善下单功能，只有未支付时才需要修改订单状态

--- Backend/trade-service/src/main/java/com/hmall/trade/listener/PayStatusListener.java
+++ Backend/trade-service/src/main/java/com/hmall/trade/listener/PayStatusListener.java
@@ -1,5 +1,6 @@
 package com.hmall.trade.listener;
 
+import com.hmall.trade.domain.po.Order;
 import com.hmall.trade.service.IOrderService;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
@@ -22,6 +23,10 @@ public class PayStatusListener {
             key = "pay.success"
     ))
     public void listenPaySuccess(Long orderId){
+        Order order = orderService.getById(orderId);
+        // 判断订单状态是否为未支付，只有未支付时才需要修改订单状态
+        if(order == null || order.getStatus() != 1) return;
+        // 标记订单状态为已支付
         orderService.markOrderPaySuccess(orderId);
     }
 }
